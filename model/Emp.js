e43f8ee1b6f33ed965982df92e125b0d5d2520e5
unknown <saxenaaman903@gmail.com>
2023-09-23 02:42:02 +0530

    commit message

---
+++ model/Emp.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+
+const EmpSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "must be provide NAME"],
+        trim: true,
+        maxlength: [20, "name  can not be more than 20"]
+    },
+    email: {
+        type: String,
+        require: true
+
+    },
+    phone: {
+        type: Number,
+        require: true
+    },
+    date: { type: Date, default: Date.now },
+
+
+})
+module.exports = new mongoose.model('TASK', EmpSchema)
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const app = express();
+
+const Empdata = require('../src/model/Emp');
+
+var bodyParser = require('body-parser')
+
+const mongoose = require("mongoose");
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/dummy', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected! successfull-------- '));
+
+
+
+app.get('/123' ,async(req,res) =>{
+    try {
+        const tasks = await Empdata.find(req.body);
+        res.status(200).json({ tasks })
+
+    } catch (error) {
+        res.status(500).json({ msg: error })
+
+    }
+})
+
+
+
+app.post('/app1' ,async(req,res) =>{
+    try {
+        const task = await Empdata.create(req.body);
+        res.status(201).json({ task });
+
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+})
+
+
+
+
+
+
+
+app.listen(4000);
