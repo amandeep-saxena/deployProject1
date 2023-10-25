aa7cc45b190b09ab7c733732eee854562b65cd88
unknown <saxenaaman903@gmail.com>
2023-10-26 03:18:51 +0530

    aman

--- index.js
+++ index.js
@@ -15,12 +15,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/dummy', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log('Connected! successfull-------- '));
+// mongoose.connect('mongodb://127.0.0.1:27017/dummy', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+//   .then(() => console.log('Connected! successfull-------- '));
 
+mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
 
 
 app.get('/123' ,async(req,res) =>{
